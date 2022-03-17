099fed986fd610d150df98c9b937e3efb33e0463
abdul <abdul.tanveer@gmail.com>
2022-03-17 11:27:13 +0530

    Initial commit

---
+++ app/src/main/java/com/abdul/harmanapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.abdul.harmanapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickHandler(View view) {
+        Toast.makeText(this, "welcom to android at harman", Toast.LENGTH_SHORT).show();
+    }
+}

1444a65348ad38c214da521470a0e32fcf3ac8a7
abdul <abdul.tanveer@gmail.com>
2022-03-17 14:32:18 +0530

    startActivity for result

--- app/src/main/java/com/abdul/harmanapp/HomeActivity.kt
+++ app/src/main/java/com/abdul/harmanapp/HomeActivity.kt
@@ -1,20 +1,38 @@
 package com.abdul.harmanapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     lateinit var tvHome: TextView
+    lateinit var etContact : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home)
         tvHome = findViewById(R.id.tvHome)
+        etContact = findViewById(R.id.etContact)
 
         var dataReceived :String
         dataReceived = intent.extras?.getString("ip").toString()
 
        // tvHome.setText(dataReceived)
         tvHome.text = dataReceived
+    }
+
+    fun contactHandler(view: View) {//2
+        //get the string from etContact
+        var contact = etContact.text.toString()
+        //put it in the intent
+        var resIntent =  Intent()
+        resIntent.putExtra("con",contact)
+        //send that intent to the parentActivity[MainActivity]
+        setResult(RESULT_OK,resIntent)
+        //close this activity
+        finish()
+
     }
 }
--- app/src/main/java/com/abdul/harmanapp/MainActivity.java
+++ app/src/main/java/com/abdul/harmanapp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.abdul.harmanapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +26,18 @@ public class MainActivity extends AppCompatActivity {
        // Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
         Intent hIntent = new Intent(MainActivity.this,com.abdul.harmanapp.HomeActivity.class);
         hIntent.putExtra("ip",input);
-        startActivity(hIntent);
+       // startActivity(hIntent);
+        ActivityCompat.startActivityForResult(this,hIntent,123,null);//1
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {//3
+        super.onActivityResult(requestCode, resultCode, data);
+        TextView resultTv = findViewById(R.id.tvResult);
+        if(resultCode == RESULT_OK && requestCode == 123){
+            String contact = data.getExtras().getString("con");
+            resultTv.setText(contact);
+        }
     }
 }

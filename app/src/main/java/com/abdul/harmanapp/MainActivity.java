e7e03e9474a77270e031035e2b5604366ea96a52
abdul <abdul.tanveer@gmail.com>
2022-03-17 11:31:39 +0530

    toast from edittext

--- app/src/main/java/com/abdul/harmanapp/MainActivity.java
+++ app/src/main/java/com/abdul/harmanapp/MainActivity.java
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etName; //declaration
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etName = findViewById(R.id.etName); //initialization or taking handle on etName
     }
 
     public void clickHandler(View view) {
-        Toast.makeText(this, "welcom to android at harman", Toast.LENGTH_SHORT).show();
+        String input = etName.getText().toString();
+        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
     }
 }

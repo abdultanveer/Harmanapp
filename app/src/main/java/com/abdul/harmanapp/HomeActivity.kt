195b287066380a90fc65daf9677730170dadbba4
abdul <abdul.tanveer@gmail.com>
2022-03-21 09:58:17 +0530

    alarm implicit intents

--- app/src/main/java/com/abdul/harmanapp/HomeActivity.kt
+++ app/src/main/java/com/abdul/harmanapp/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.abdul.harmanapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
@@ -24,15 +25,36 @@ class HomeActivity : AppCompatActivity() {
     }
 
     fun contactHandler(view: View) {//2
+        when(view.id){
+            R.id.button2 -> {sendContact()}
+            R.id.btnAlarm -> { createAlarm("studytime",10,0)}
+        }
+
+
+
+    }
+
+    private fun sendContact() {
         //get the string from etContact
         var contact = etContact.text.toString()
         //put it in the intent
-        var resIntent =  Intent()
-        resIntent.putExtra("con",contact)
+        var resIntent = Intent()
+        resIntent.putExtra("con", contact)
         //send that intent to the parentActivity[MainActivity]
-        setResult(RESULT_OK,resIntent)
+        setResult(RESULT_OK, resIntent)
         //close this activity
         finish()
+    }
 
+
+    fun createAlarm(message: String, hour: Int, minutes: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_HOUR, hour)
+            putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 }

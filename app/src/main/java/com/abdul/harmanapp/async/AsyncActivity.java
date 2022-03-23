3e2e5a38ce4c9d1718a2478887dbd66eb8c7bc8f
abdul <abdul.tanveer@gmail.com>
2022-03-23 12:13:05 +0530

    notifications

--- app/src/main/java/com/abdul/harmanapp/async/AsyncActivity.java
+++ app/src/main/java/com/abdul/harmanapp/async/AsyncActivity.java
@@ -1,8 +1,12 @@
 package com.abdul.harmanapp.async;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -11,6 +15,7 @@ import com.abdul.harmanapp.R;
 
 public class AsyncActivity extends AppCompatActivity {
 ProgressBar mProgressBar;
+    NotificationManager notificationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +38,22 @@ ProgressBar mProgressBar;
                 Intent servIntent = new Intent(this,MusicService.class);
                 stopService(servIntent);
                 break;
+            case R.id.btnNotify:
+                showNotification();
+                break;
         }
+    }
+
+    private void showNotification() {
+        createNotificationChannel();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "harman_imp_ID")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("harman title")
+                .setContentText("android app dev")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        notificationManager.notify(12, builder.build()); //id 12 -- is notfication id to update or cancel a notification
+
     }
 
     private void downloadMovie() {
@@ -41,5 +61,27 @@ ProgressBar mProgressBar;
         DownloadTask downloadTask = new DownloadTask(mProgressBar);
         downloadTask.execute("http://mymoviedownloadurl.com");
         //pass the download url to it
+    }
+
+    /**
+     * channel was introduced to prevent spamming notice board
+     * you can disable a channel from settings
+     */
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "ride updates";
+                    //getString(R.string.channel_name);
+            String description = "this channel sends the important updates about the ride";
+                    //getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("harman_imp_ID", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+             notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }

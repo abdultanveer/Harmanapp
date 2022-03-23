7a891ef035c6dff4642bef9e4f5bf097c540ef87
abdul <abdul.tanveer@gmail.com>
2022-03-23 10:26:47 +0530

    asynctask-- part 2 -- publish progress

--- app/src/main/java/com/abdul/harmanapp/async/DownloadTask.java
+++ app/src/main/java/com/abdul/harmanapp/async/DownloadTask.java
@@ -7,6 +7,7 @@ import android.widget.ProgressBar;
 /**
  * background thread to download a movie
  */
+//params-i/p, progress type, result type}
 public class DownloadTask extends AsyncTask<String,Integer,Void> {
     public static String TAG = DownloadTask.class.getSimpleName();
     ProgressBar mProgressBar;//declaration
@@ -16,9 +17,25 @@ public class DownloadTask extends AsyncTask<String,Integer,Void> {
     }
 
     @Override
-    protected Void doInBackground(String... strings) {
+    protected Void doInBackground(String... strings) { //worker thread doing the job in background
         Log.i(TAG,strings[0]);
-        mProgressBar.setProgress(50);
+        try {
+            for(int i=1;i<20;i++){
+                Thread.sleep(200);
+                publishProgress(i*5);
+
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         return null;
-    } //params-i/p, progress type, result type}
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        mProgressBar.setProgress(values[0]);
+
+    }
 }

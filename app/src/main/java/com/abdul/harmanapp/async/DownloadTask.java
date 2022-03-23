55f1b3923bdd3d0cfda8504923ceab0cdd60a408
abdul <abdul.tanveer@gmail.com>
2022-03-23 10:19:57 +0530

    asynctask-- part 2 -- updating the progress bar

--- app/src/main/java/com/abdul/harmanapp/async/DownloadTask.java
+++ app/src/main/java/com/abdul/harmanapp/async/DownloadTask.java
@@ -1,17 +1,24 @@
 package com.abdul.harmanapp.async;
 
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.ProgressBar;
 
 /**
  * background thread to download a movie
  */
 public class DownloadTask extends AsyncTask<String,Integer,Void> {
+    public static String TAG = DownloadTask.class.getSimpleName();
+    ProgressBar mProgressBar;//declaration
+
     public DownloadTask(ProgressBar progressBar) {
+        mProgressBar = progressBar; //instantiation
     }
 
     @Override
     protected Void doInBackground(String... strings) {
+        Log.i(TAG,strings[0]);
+        mProgressBar.setProgress(50);
         return null;
     } //params-i/p, progress type, result type}
 }
